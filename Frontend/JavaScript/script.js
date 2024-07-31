aa9018f8af0d5adbbff822f0506ed94ffd41f931
Lodi <joaolodi01@gmail.com>
2024-07-30 23:38:42 -0300

    Menu bar

--- Frontend/JavaScript/script.js
+++ Frontend/JavaScript/script.js
@@ -1,25 +1,42 @@
-openMenu.addEventListner('click', () =>{
-    menu.style.display = 'flex'
+class MobileNavBar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
 
-    menu.style.right = (menu.offsetWidth * -1) + 'px'
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-    openMenu.style.display = 'none'
+    animateLinks(){
+        this.navLinks.forEach((link, index) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
+        });
+    }
 
-    setTimeout(() =>{
-        menu.style.opacity = '1'
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
 
-        menu.style.right = '0'
-    }, 10)
-})
+    addClickEvent(){
+        this.mobileMenu.addEventListener('click', this.handleClick);
+    }
 
-closeMenu.addEventListner('click', () =>{
-    menu.style.opacity = '0'
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
 
-    menu.style.right = (menu.offsetWidth * -1) + 'px'
-
-    setTimeout(() =>{
-        menu.removeAttribute('style')
-        openMenu.removeAttribute('style')
-    }, 200)
-    menu.removeAttribute('style')
-})
+const mobileNavBar = new MobileNavBar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+mobileNavBar.init();
